1f9c06953486e1667ed0bb8865b9db1bf6b9c962
Freddie <fthompson803@live.com>
2020-01-31 23:42:39 -0500

    adding migrations

---
+++ migrations/20200131232936_cars.js
@@ -0,0 +1,18 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments();
+    tbl
+      .text('VIN', 128)
+      .unique()
+      .notNullable();
+    tbl.text('Make', 128).notNullable();
+    tbl.text('Model', 128).notNullable();
+    tbl.integer('Mileage', 15).notNullable();
+    tbl.text('Transmission', 128);
+    tbl.text('Title Status', 128);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.shema.dropTableIfExists('cars');
+};
